a9a4d5ea55ad0f48552946005d859b8f9639bee6
Carlos Anibal Juarez <cjuarez@uca.edu.sv>
2023-02-14 16:45:33 -0600

    version 2013

---
+++ U3 Excitaciones/parte3_DC1A_data.m
@@ -0,0 +1,18 @@
+%parte3_DC1A_data.m
+
+KA=187;
+TA=0.89;
+TE=1.15;
+KE=1;
+AEX=0.014;
+BEX=1.55;
+
+KF=0.058;
+TF=0.62;
+
+TB=0.06;
+TC=0.173;
+VRMAX=1.7;
+VRMIN=-1.7;
+
+VUEL=-0.01;% no puede ser cero.
